a4e57ccb8a5a22001533d3b5320a57c99ad5bf4a
swj <159632_@naver.com>
2021-12-04 09:01:18 +0900

    fix: test code

--- src/test/java/com/cad/book/springboot/domain/PostsRepositoryTest.java
+++ src/test/java/com/cad/book/springboot/domain/PostsRepositoryTest.java
@@ -1,61 +1,71 @@
 package com.cad.book.springboot.domain;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class PostsRepositoryTest {
 
-//  @Autowired
-//  PostsRepository postsRepository;
-//
-//  @After
-//  public void cleanup() {
-//    postsRepository.deleteAll();
-//  }
+import com.cad.book.springboot.domain.posts.Posts;
+import com.cad.book.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//  @Test
-//  public void open_saved_File() {
-//    // given
-//    String title = "테스트 게시글";
-//    String content = "테스트 본문";
-//
-//    /**
-//     * posts에 insert/update 실행 id 값 有:update / 無:insert
-//     */
-//    postsRepository.save(Posts.builder()
-//        .title(title)
-//        .content(content)
-//        .author("cadqe13@gmail.com")
-//        .build());
-//
-//    // when
-//    List<Posts> postsList = postsRepository.findAll();
-//
-//    // then
-//    Posts posts = postsList.get(0);
-//    assertThat(posts.getTitle()).isEqualTo(title);
-//    assertThat(posts.getContent()).isEqualTo(content);
-//  }
-//
-//  @Test
-//  public void BaseTimeEntity_등록() {
-//    // given
-//    LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
-//    postsRepository.save(Posts.builder()
-//        .title("title")
-//        .content("content")
-//        .author("author")
-//        .build());
-//
-//    // when
-//    List<Posts> postsList = postsRepository.findAll();
-//
-//    // then
-//    Posts posts = postsList.get(0);
-//
-//    System.out.println(">>>>>>>>> createdDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
-//
-//    assertThat(posts.getCreatedDate()).isAfter(now);
-//    assertThat(posts.getModifiedDate()).isAfter(now);
-//  }
+import java.time.LocalDateTime;
+import java.util.List;
 
-//}
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+  @Autowired
+  PostsRepository postsRepository;
+
+  @After
+  public void cleanup() {
+    postsRepository.deleteAll();
+  }
+
+  @Test
+  public void 게시글저장_불러오기() {
+    //given
+    String title = "테스트 게시글";
+    String content = "테스트 본문";
+
+    postsRepository.save(Posts.builder()
+            .title(title)
+            .content(content)
+            .author("jojoldu@gmail.com")
+            .build());
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+    assertThat(posts.getTitle()).isEqualTo(title);
+    assertThat(posts.getContent()).isEqualTo(content);
+  }
+
+  @Test
+  public void BaseTimeEntity_등록() {
+    //given
+    LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+    postsRepository.save(Posts.builder()
+            .title("title")
+            .content("content")
+            .author("author")
+            .build());
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+
+    System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+    assertThat(posts.getCreatedDate()).isAfter(now);
+    assertThat(posts.getModifiedDate()).isAfter(now);
+  }
+}
--- src/test/java/com/cad/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/cad/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,26 +1,23 @@
 package com.cad.book.springboot.web.dto;
 
 
-//public class HelloResponseDtoTest {
-//
-//  @Test
-//  public void lombok_test() {
-//
-//    //given
-//    String name = "test";
-//    int amount = 1000;
-//
-//    //when
-//    HelloResponseDto dto = new HelloResponseDto(name, amount);
-//
-//    //then
-//    /**
-//     * assertj 의 assertThat 의 장점
-//     * 1. CoreMatchers 와 달리 추가적으로 라이브러리가 필요하지 않다.
-//     * 2. 자동 완성이 좀 더 확실하게 지원된다.
-//     */
-//    assertThat(dto.getName()).isEqualTo(name);
-//    assertThat(dto.getAmount()).isEqualTo(amount);
-//  }
-//
-//}
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+  @Test
+  public void 롬복_기능_테스트() {
+    //given
+    String name = "test";
+    int amount = 1000;
+
+    //when
+    HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+    //then
+    assertThat(dto.getName()).isEqualTo(name);
+    assertThat(dto.getAmount()).isEqualTo(amount);
+  }
+}
